3471e012fa6442f8e80320a87ecc0e35c78a16c9
YSM-0 <120345032+YSM-0@users.noreply.github.com>
2023-01-09 19:38:40 +0100

    all the necessary files are up the simple functions add, subtract, multiply and divide are up

---
+++ script.js
@@ -0,0 +1,16 @@
+const add = function(a, b) {
+    return a + b;
+}
+
+const subtract = function(a, b) {
+    return a - b;
+}
+
+const multiply = function(a, b) {
+    return a * b;
+}
+
+const divide = function(a, b) {
+    return a / b;
+}
+
